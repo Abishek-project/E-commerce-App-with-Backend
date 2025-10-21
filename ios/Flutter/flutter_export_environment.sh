7464bd70e6734176c75afa0391bbfcf11baba57c
abishek.j <abishek.j@kobil.com>
2025-10-21 12:43:15 +0530

    Deploy Flutter web to GitHub Pages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/abishek/fvm/versions/3.19.0"
+export "FLUTTER_APPLICATION_PATH=/Users/abishek/personal/E-commerce-App-with-Backend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
